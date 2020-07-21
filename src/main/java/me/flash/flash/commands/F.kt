25399addc0e8a06db17a67d757063b22dd72fd9c
JGamingz <me@jgamingz.ml>
2020-07-22 00:46:53 +0100

    Hotfix (Permissions check was wrong)

--- src/main/java/me/flash/flash/commands/F.kt
+++ src/main/java/me/flash/flash/commands/F.kt
@@ -10,7 +10,9 @@ class F : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if (!sender.hasPermission("core.f")) {
             sender.sendMessage(Flash.noPermission)
-        } else if (args.isEmpty()){
+            return true
+        }
+        if (args.isEmpty()){
             Bukkit.getOnlinePlayers().forEach { player->
                 player.chat("F")
             }
