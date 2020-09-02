d3f74b9981b55e387bfb30c552b0c79b9954cf58
DarrenSanders123 <49994636+DarrenSanders123@users.noreply.github.com>
2020-09-02 10:29:29 +0200

    Update Kill.kt

--- src/main/java/me/flash/flash/commands/Kill.kt
+++ src/main/java/me/flash/flash/commands/Kill.kt
@@ -19,7 +19,7 @@ class Kill : CommandExecutor{
             return true
         }
         val player = Bukkit.getPlayer(args.first()) ?: sender.sendMessage(targetOffline).let { return true }
-        player.setHealth(0.0D);
+        player.setHealth(0.0)
         sender.sendMessage("Killed ${player.name}")
         return true
     }
