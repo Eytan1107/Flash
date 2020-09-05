d6c293daa7be1abbf9bceb50be3693cb386f5a35
JGamingz <me@jgamingz.ml>
2020-09-05 21:18:34 +0100

    Gamemode commands

---
+++ src/main/java/me/flash/flash/commands/GameMode.kt
@@ -0,0 +1,55 @@
+package me.flash.flash.commands
+
+import me.flash.flash.Flash
+import me.flash.flash.Flash.Companion.error
+import org.bukkit.Bukkit
+import org.bukkit.GameMode
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+class GameMode : CommandExecutor {
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (args.isEmpty()) {
+            if (sender !is Player) sender.sendMessage("&cUsage : /gamemode [gamemode] [player]").run { return true }
+            else sender.sendMessage("&cUsage : /gamemode [gamemode] <player>").run { return true }
+        } else if (args.size == 1) {
+            if (sender !is Player) sender.sendMessage("&cUsage : /gamemode [gamemode] [player]").run { return true }
+            if (!sender.hasPermission("flash.gamemode")) sender.sendMessage(Flash.noPermission).run { return true }
+            else {
+                val gameMode = parseGamemode(args.first()) ?: sender.sendMessage("That is not a valid gamemode!".error()).run { return true }
+                sender.gameMode = gameMode
+                Flash.staffMessage(sender.name, "Set their gamemode to ${gameMode.name.toLowerCase()} mode.")
+            }
+        } else {
+            val gameMode = parseGamemode(args.first()) ?: sender.sendMessage("That is not a valid gamemode!".error()).run { return true }
+            val player = Bukkit.getPlayer(args[1]) ?: sender.sendMessage(Flash.targetOffline).run { return true }
+            player.gameMode = gameMode
+            Flash.staffMessage(sender.name, "Set ${player.name}'s gamemode to ${gameMode.name.toLowerCase()} mode.")
+            player.sendMessage("Your gamemode was set to ${gameMode.name.toLowerCase()} mode by ${sender.name}")
+            sender.sendMessage("You set ${player.name}'s gamemode to ${gameMode.name.toLowerCase()}")
+        }
+        return true
+    }
+
+    private fun parseGamemode(arg: String) : GameMode? {
+        return when (arg) {
+            "creative", "c", "1" -> {
+                GameMode.CREATIVE
+            }
+            "survival", "s", "0" -> {
+                GameMode.SURVIVAL
+            }
+            "adventure", "a", "2" -> {
+                GameMode.ADVENTURE
+            }
+            "spectator", "sp", "3" -> {
+                GameMode.SPECTATOR
+            }
+            else -> {
+                null
+            }
+        }
+    }
+}
---
+++ src/main/java/me/flash/flash/commands/ShGameMode.kt
@@ -0,0 +1,49 @@
+package me.flash.flash.commands
+
+import me.flash.flash.Flash
+import me.flash.flash.Flash.Companion.error
+import me.flash.flash.Flash.Companion.prefix
+import org.bukkit.Bukkit
+import org.bukkit.command.Command
+import org.bukkit.GameMode
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+class ShGameMode : CommandExecutor {
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (!sender.hasPermission("flash.gamemode")) sender.sendMessage(Flash.noPermission).run { return true }
+        if (args.isEmpty()) {
+            sender.sendMessage("&cUsage: /gamemode [gamemode] <player>".prefix())
+        } else if (args.size == 1) {
+            if (sender !is Player) sender.sendMessage("&cUsage: /gamemode [gamemode] [player]".prefix()).run { return true }
+            else {
+                val gameMode = parseGamemode(args.first())
+                        ?: sender.sendMessage("The command you ran is not a valid gamemode command.".error()).run { return true }
+                sender.gameMode = gameMode
+                sender.sendMessage(("You set your gamemode to ${gameMode.name.toLowerCase()} mode.").prefix())
+                Flash.staffMessage(sender.name, "Set their gamemode to ${gameMode.name.toLowerCase()} mode")
+            }
+        } else {
+            val player = Bukkit.getPlayer(args[1]) ?: sender.sendMessage(Flash.targetOffline).run { return true }
+            val gameMode = parseGamemode(args.first())
+                    ?: sender.sendMessage("The command you ran is not a valid gamemode command.".error()).run { return true }
+            player.gameMode = gameMode
+            sender.sendMessage("You set ${player.name}'s gamemode to ${gameMode.name.toLowerCase()} mode.")
+            player.sendMessage("${sender.name} set your gamemode to ${gameMode.name.toLowerCase()} mode.")
+            Flash.staffMessage(sender.name, "Set ${player.name}'s gamemode to ${gameMode.name.toLowerCase()} mode")
+        }
+        return true
+    }
+
+    private fun parseGamemode(arg: String): GameMode? {
+        return when (arg) {
+            "gmsp" -> GameMode.SPECTATOR
+            "gmc" -> GameMode.CREATIVE
+            "gms" -> GameMode.SURVIVAL
+            "gma" -> GameMode.ADVENTURE
+            else -> null
+        }
+    }
+
+}
