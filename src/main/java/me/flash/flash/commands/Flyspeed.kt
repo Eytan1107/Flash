9148cae8823397ffb71ce80946e42875446f07a8
DarrenSanders123 <Darrengk8y%$>
2020-09-07 13:18:27 +0200

    changes the collor

--- src/main/java/me/flash/flash/commands/Flyspeed.kt
+++ src/main/java/me/flash/flash/commands/Flyspeed.kt
@@ -11,6 +11,7 @@ import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.bukkit.ChatColor
 
 class Flyspeed : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
@@ -19,7 +20,7 @@ class Flyspeed : CommandExecutor {
         val speed = args.firstOrNull()?.toIntOrNull() ?: sender.sendMessage("&cUsage: /flyspeed <speed>".prefix()).run { return true }
         if (!IntRange(1, 10).contains(speed)) sender.sendMessage("Please specify a whole number 1 and 10.".error()).run { return true }
         sender.flySpeed = speed.toFloat()/10
-        sender.sendMessage("Set flight speed for &l${sender.name} &6to $speed".prefix())
+        sender.sendMessage("Set flight speed for &l${sender.name}&r &6to &l$speed".prefix())
         Flash.staffMessage(sender, "Set their flight speed to $speed".colour())
         return true
     }
