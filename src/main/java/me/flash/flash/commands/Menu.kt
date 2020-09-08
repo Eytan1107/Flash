c58999f835caaded55188f6f7ec53b986510fcbb
DarrenSanders123 <Darrengk8y%$>
2020-09-08 09:52:40 +0200

    testing the inventory

---
+++ src/main/java/me/flash/flash/commands/Menu.kt
@@ -0,0 +1,18 @@
+package me.flash.flash.commands
+
+import org.bukkit.Bukkit
+import org.bukkit.Material
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.inventory.*
+import org.bukkit.inventory.Inventory
+
+class Menu : CommandExecutor {
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        val inventory = Bukkit.createInventory(null, 27, "server selctor")
+        inventory.setItem(0, ItemStack(Material.GOLD_BLOCK, 1))
+        return true
+        }
+    }
+
