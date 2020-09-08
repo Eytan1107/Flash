d307cee0bd281c46279128e22264de45f5abe935
DarrenSanders123 <Darrengk8y%$>
2020-09-08 15:15:05 +0200

    testing the inventory

--- src/main/java/me/flash/flash/commands/Menu.kt
+++ src/main/java/me/flash/flash/commands/Menu.kt
@@ -1,27 +1,30 @@
 package me.flash.flash.commands
 
 import org.bukkit.Bukkit
+//import org.bukkit.ChatColor
 import org.bukkit.Material
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
-import org.bukkit.event.EventHandler
 import org.bukkit.inventory.*
-import org.bukkit.inventory.Inventory
-import org.bukkit.event.inventory.InventoryClickEvent
-import org.bukkit.event.inventory.InventoryType
 import org.bukkit.event.Listener
-
 
 class Menu : CommandExecutor, Listener {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         val inventory = Bukkit.createInventory(null, 27, "server selector")
-        inventory.setItem(0, ItemStack(Material.GOLD_BLOCK, 1))
+        val dirt = ItemStack(Material.DIRT,1)
+        val dirtMeta = dirt.itemMeta
+        dirtMeta.lore[0] = "test"
+        dirt.itemMeta = dirtMeta
+
+        inventory.setItem(0, dirt)
         val player = Bukkit.getPlayer(sender.name)
         player.openInventory(inventory)
+
         return true
         }
     }
+
 
 
     
