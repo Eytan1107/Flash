9c7f2aaf5cd2fe1c0598f841103d9adf0e003d9d
DarrenSanders123 <Darrengk8y%$>
2020-09-08 15:18:45 +0200

    testing the inventory

--- src/main/java/me/flash/flash/commands/Menu.kt
+++ src/main/java/me/flash/flash/commands/Menu.kt
@@ -7,14 +7,14 @@ import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.inventory.*
-import org.bukkit.event.Listener
 
-class Menu : CommandExecutor, Listener {
+
+class Menu : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         val inventory = Bukkit.createInventory(null, 27, "server selector")
         val dirt = ItemStack(Material.DIRT,1)
         val dirtMeta = dirt.itemMeta
-        dirtMeta.lore[0] = "test"
+        dirtMeta.getLore().set(0, "test")
         dirt.itemMeta = dirtMeta
 
         inventory.setItem(0, dirt)
