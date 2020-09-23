40aa5c6d180e6fd8b2ae2fe32e03dd5a5953daaa
Flash <eytansebag1@gmail.com>
2020-09-23 23:10:41 +0300

    msg

--- src/main/java/me/flash/flash/commands/Reply.kt
+++ src/main/java/me/flash/flash/commands/Reply.kt
@@ -6,14 +6,16 @@ import me.flash.flash.Flash.Companion.error
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import org.bukkit.command.ConsoleCommandSender
 import org.bukkit.entity.Player
 
 class Reply : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
-        val player = Msg.lastMessaged[sender].let {
-            if (it !is Player) sender.sendMessage("You cannot reply to the console".error()).run { return true }
+        val player = Msg.lastMessaged[sender]?.let {
+            if (it is ConsoleCommandSender) sender.sendMessage("You cannot reply to the console".error()).run { return true }
+            else if (it !is Player) sender.sendMessage("Pretty interesting that you got a message from an animal, can't reply to it though :)".error()).run { return true }
             else it
-        }
+        } ?: sender.sendMessage("Nobody to reply to".error()).run { return true }
         val senderPrefix = if (sender is Player) Flash.vaultChat.getPlayerPrefix(sender) else "&4".color()
         val playerPrefix = Flash.vaultChat.getPlayerPrefix(player)
         sender.sendMessage("&7(&aTo $playerPrefix${player.name}&7)&a ".color() + args.toMutableList().joinToString (" "))
