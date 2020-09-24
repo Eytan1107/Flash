adbb32b3cc7faf9a3616811984d81a276a6a7515
Flash <eytansebag1@gmail.com>
2020-09-25 02:25:21 +0300

    fixed hub

--- src/main/java/me/flash/flash/commands/SetHub.kt
+++ src/main/java/me/flash/flash/commands/SetHub.kt
@@ -12,7 +12,7 @@ class SetHub : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if (sender !is Player) sender.sendMessage("Huh? Where you wanna set the hub ? In the files ?".error()).run { return true }
         if (args.isNotEmpty()) sender.sendMessage("Too many arguments".error())
-        if (sender.world.name !== "world") sender.sendMessage("You can only do this command in the Hub.".error()).run { return true }
+        if (sender.world.name != "world") sender.sendMessage("You can only do this command in the Hub.".error()).run { return true }
         with(sender.location) {
             sender.world.setSpawnLocation(x.toInt(), y.toInt(), z.toInt())
             sender.sendMessage("Hub set at : &c".prefix() + x.toInt() + ", " + y.toInt() + ", " + z.toInt())
